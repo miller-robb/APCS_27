212bd82a7f573034b0c0e95b3815c84e3ba9dba6
miller-robb <millercrobb@gmail.com>
2026-09-03 21:17:15 +0000

    I have completed lab 3 and 4

--- 000_Labs/004_String_class/base_code/starter.java
+++ 000_Labs/004_String_class/base_code/starter.java
@@ -9,6 +9,12 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		String dog = new String("Happy Birthday, to you\nHappy Birthday, to you\nHappy Birthday dear ");
+		String cat = new String("proper name of person in this class");
+		String turtle = new String("Happy Birthday, to you");
+		System.out.print(dog);
+		System.out.println(cat);
+		System.out.println(turtle);
+
 	}
 }
