6b99e40162774c7929e81807d2911952509f613d
miller-robb <millercrobb@gmail.com>
2026-09-10 21:25:37 +0000

    finished lab 9

--- 000_Labs/009_math_methods/base_code/starter.java
+++ 000_Labs/009_math_methods/base_code/starter.java
@@ -7,8 +7,17 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+	double dog;
+	dog=Math.max(13-6*11,30%7*(-2));
+	System.out.println("Maximum = "+dog);
+	double cat;
+	cat=Math.sqrt(3*8+31%7);
+	System.out.println("Square root = "+cat);
+	double turtle;
+	turtle=Math.pow(37/3,35%21);
+	System.out.println("Power = "+turtle);
+	double hampster;
+	hampster=Math.max(Math.pow(2,14%3),Math.sqrt(2*6));
+	System.out.println("Max = "+hampster);
 	}
 }
